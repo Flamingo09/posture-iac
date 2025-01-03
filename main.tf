7168f742cf8047060c92fd6bd77ab9cffb064868
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-03 13:25:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,41 @@ provider "google" {
   project = "terraform-cloud-445206"
   region  = "global"
 }
+
+resource "google_storage_bucket" "good_bucket" {
+  name          = "good_bucket"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
+resource "google_storage_bucket" "bad_bucket" {
+  name          = "bad_bucket"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = false
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
