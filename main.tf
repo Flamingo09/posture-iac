4b2497be336c6f731f69128b0d06be6a98aba886
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-04 22:34:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = "global"
 }
 
-resource "google_storage_bucket" "good_bucket_1" {
+resource "google_storage_bucket" "good_bucket" {
   name          = "good_bucket"
   location      = "EU"
   force_destroy = true
