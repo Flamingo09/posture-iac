e79bd9ebddc1ffc8bf832d00358ef63c20b1a3f3
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-05 21:09:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,29 @@ provider "google" {
   region  = "global"
 }
 
+module "gcp_org_policy_v2" {
+ source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+
+ policy_root      = "organization"
+ policy_root_id   = "9454078371"
+ constraint       = "iam.disableServiceAccountKeyUpload"
+ policy_type      = "boolean"
+ exclude_folders  = []
+ exclude_projects = []
+
+
+ rules = [
+   # Rule 1
+   {
+    // Modification(enforcement - false ->true).
+     enforcement = true
+     allow       = []
+     deny        = []
+     conditions  = []
+   },
+ ]
+}
+
 resource "google_org_policy_policy" "primary" {
  name   = "projects/302158293184/policies/compute.requireOsLogin"
  parent = "projects/302158293184"
