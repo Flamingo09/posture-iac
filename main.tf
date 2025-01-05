254c2cefcbe843b352d9f9aa86af773e3c1949f6
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-05 21:25:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,3 @@ provider "google" {
   region  = "global"
 }
 
-resource "google_storage_bucket" "ak-tf-test" {
-  name          = "ak-tf-test"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = false
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
-}
