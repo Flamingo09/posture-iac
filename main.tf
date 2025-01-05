78f94ad4a52e876858107cd1dc2b832cd2827080
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-05 21:40:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 provider "google" {
-  project = "terraform-cloud-445206"
+  project = "terraform-cloud-445206-1"
   region  = "global"
-}
-
-resource "google_compute_network_1" "network1"{
-  name                                        = "network-vpc1"
-  delete_default_routes_on_create = false
-  auto_create_subnetworks           = false
-  routing_mode                      = "REGIONAL"
-  mtu = 1301
-  project = "terraform-cloud-445206"
 }
