17bd2626852574b20f4aa4bb031c4498d4c875b3
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-05 21:13:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,25 @@ provider "google" {
   region  = "global"
 }
 
+resource "google_storage_bucket" "ak-tf-test" {
+  name          = "ak-tf-test"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = false
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
 module "gcp_org_policy_v2" {
  source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
 
