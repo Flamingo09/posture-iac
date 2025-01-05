8d578030c77f088fca12707426c82cba9fb4f187
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-05 12:16:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,26 @@ resource "google_storage_bucket" "akanksha_tf_testing" {
   location      = "EU"
   force_destroy = true
 
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
+resource "google_storage_bucket" "akanksha_tf_testing_1" {
+  name          = "akanksha_tf_testing_1"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = false
 
   website {
     main_page_suffix = "index.html"
