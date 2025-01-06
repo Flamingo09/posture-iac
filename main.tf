e07e1363bfb25db657667901cb8a6dd9e752b5a2
Akanksha Singh <54772819+Flamingo09@users.noreply.github.com>
2025-01-06 11:52:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,42 @@ provider "google" {
   region  = "global"
 }
 
-resource "google_securityposture_posture" "posture_2" {
-  posture_id  = "posture_2"
-  parent      = "organizations/ewir7yi78437"
-  location    = "global"
-  state       = "ACTIVE"
-  description = "a new posture"
-  policy_sets {
-      policy_set_id = "org_policy_set"
-      description   = "set of org policies"
-      policies {
-          policy_id = "policy_1"
-          constraint {
-              org_policy_constraint {
-                  canned_constraint_id = "storage.uniformBucketLevelAccess"
-                  policy_rules {
-                      enforce = true
-                  }
-              }
-          }
-      }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "7a7b34d9-6f14-482e-90df-44be6bef083d"
+    tenant_id = "c74f9791-f0d8-4764-826d-f0393bafd5b9"
+    features {
+    }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = "westeurope"
+  resource_group_name = "example-resources"
+
+  security_rule {
+    name                       = "test123"
+    description                = "security_rule description"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    source_application_security_group_ids = ["acceptanceTestSecurityGroup1"]
+  }
+
+  tags = {
+    environment = "Production"
   }
 }
